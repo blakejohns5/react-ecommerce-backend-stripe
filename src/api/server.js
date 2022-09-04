cfa550311abd8e4d77157f6f89983d1d4e7648d8
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-09-04 23:09:33 +0200

    add stripe functionality, stripe checkout session

--- src/api/server.js
+++ src/api/server.js
@@ -1,27 +1,45 @@
-
-
-console.log('hello world again')
+require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
+app.use(express.json());
+
 const cors = require('cors');
 app.use(cors({
     origin: '*',
 }))
 
-app.post('/create-checkout-session', (req, res) => {
-    console.log('HELLO WORLD')
-    res.status(200).send({
-        msg: 'HELLO WORLD',
-        msg2: 'AGAIN'
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: req.body.cartItems.map(item => {
+          const priceInCents = item.price * 100;
+          return {
+              price_data: {
+                  currency: 'eur',
+                  product_data: {
+                      name: item.name
+                  },
+                  unit_amount: priceInCents,
+              },
+              quantity: item.qty,
+          }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
     })
-    
+    res.json({ url: session.url })
+  } catch (error) {
+      res.status(500).json({
+          error: error.message
+      })
+  }
 })
-
-// app.get('/ingredients', (req, res) =>{
-//     res.send(ingredients);
-// });
-
 
 const PORT = 4242;
 
